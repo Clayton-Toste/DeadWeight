0fd8918708651603d7c8aad1cd991895fb2890a6
Kali Toste <claytontoste@gmail.com>
2024-08-18 22:50:05 -0700

    Intro

--- Assets/Scripts/Jukebox.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Jukebox : MonoBehaviour
-{
-    public void PlaySound(Object sound)
-    {
-        AudioSource.PlayClipAtPoint(sound as AudioClip, transform.position, 0.2f);
-    }
-}
